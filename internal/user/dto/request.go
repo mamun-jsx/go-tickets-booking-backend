23d29a488b9f3b7902fa7a615d97ca59d68f58aa
almamun528 <ayapths@gmail.com>
2026-09-20 16:11:13 +0600

    write dto request & response

--- internal/user/dto/request.go
+++ internal/user/dto/request.go
@@ -1,1 +1,8 @@
 package dto
+
+// by this we will transfer data
+type CreateRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=12"`
+}
--- internal/user/dto/response.go
+++ internal/user/dto/response.go
@@ -1,1 +1,10 @@
 package dto
+
+// send the response to the client but not password
+type Response struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	CreatedAt string `json:"created_at"`
+	
+}
