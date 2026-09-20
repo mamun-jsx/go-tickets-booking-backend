be93ce2416fe1cbf2ba2d2349085e4bd7911e653
almamun528 <ayapths@gmail.com>
2026-09-20 16:26:45 +0600

    write repository file logic

--- internal/user/entity.go
+++ internal/user/entity.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" validate:"required" gorm:"type:varchar(255); not null;"`
-	Email    string `json:"email" validate:"required,email" gorm:"type:varchar(255);uniqueIndex; not null;"`
-	Password string `json:"password" validate:"required,min=6,max=12" gorm:"type:varchar(255); not null;"`
+	Name     string `json:"name" gorm:"type:varchar(255); not null;"`
+	Email    string `json:"email" gorm:"type:varchar(255);uniqueIndex; not null;"`
+	Password string `json:"password" gorm:"type:varchar(255); not null;"`
 }
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,42 @@
 package user
+
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+// keep the core things here like database interaction with user
+// dont write any http related code here
+
+var ErrorAlreadyExist = errors.New("user with this email already exists")
+
+type Repository interface {
+	// create a user
+	CreateUser(user *User) error
+}
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+// create a user
+func (r repository) CreateUser(user *User) error {
+	result := r.db.Create(user)
+	if result.Error != nil {
+
+		// handle error via error package not string
+		if errors.Is(result.Error, gorm.ErrDuplicatedKey) {
+
+			return ErrorAlreadyExist
+		}
+
+		return result.Error
+	}
+	return nil
+}
