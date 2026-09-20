660752187dfddd26c6f45b4d953e29fe70a44fb1
almamun528 <ayapths@gmail.com>
2026-09-20 16:41:27 +0600

    write service layer for user create func

--- internal/user/dto/request.go
+++ internal/user/dto/request.go
@@ -2,6 +2,7 @@ package dto
 
 // by this we will transfer data
 type CreateRequest struct {
+	
 	Name     string `json:"name" validate:"required"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=6,max=12"`
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,35 @@
 package user
+
+import "github.com/mamun-jsx/go-tickets-booking-backend.git/internal/user/dto"
+
+type service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) *service {
+	return &service{
+		repo: repo,
+	}
+}
+
+
+// create user service 
+func (s *service) CreateUser(req dto.CreateRequest) (*dto.Response, error) {
+	// * take the repo create user function and call it and return the error
+	user := User{
+		Name:     req.Name,
+		Email:    req.Email,
+		Password: req.Password,
+	}
+	err := s.repo.CreateUser(&user)
+	if err != nil {
+		return nil, err
+	}
+	response := dto.Response{
+		ID:        user.ID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt.String(),
+	}
+	return &response, nil
+}
